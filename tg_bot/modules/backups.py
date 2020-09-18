c6cd92ef63d05ae0a742a836a13780518076846c
sairbey <70520676+sairbey@users.noreply.github.com>
2020-09-18 13:56:49 +0300

    Update backups.py

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -10,9 +10,8 @@ from tg_bot import dispatcher, LOGGER
 from tg_bot.__main__ import DATA_IMPORT
 from tg_bot.modules.helper_funcs.chat_status import user_admin
 
+@sairbey 
 
-@run_async
-@user_admin
 def import_data(bot: Bot, update):
     msg = update.effective_message  # type: Optional[Message]
     chat = update.effective_chat  # type: Optional[Chat]
@@ -22,8 +21,8 @@ def import_data(bot: Bot, update):
         try:
             file_info = bot.get_file(msg.reply_to_message.document.file_id)
         except BadRequest:
-            msg.reply_text("Try downloading and reuploading the file as yourself before importing - this one seems "
-                           "to be iffy!")
+            msg.reply_text("İçe aktarmadan önce dosyayı kendiniz gibi indirmeyi ve yeniden yüklemeyi deneyin - bu görünüyor"
+                           "şüpheli olmak!")
             return
 
         with BytesIO() as file:
@@ -33,8 +32,8 @@ def import_data(bot: Bot, update):
 
         # only import one group
         if len(data) > 1 and str(chat.id) not in data:
-            msg.reply_text("Theres more than one group here in this file, and none have the same chat id as this group "
-                           "- how do I choose what to import?")
+            msg.reply_text("Bu dosyada birden fazla grup var ve hiçbiri bu grupla aynı sohbet kimliğine sahip değil "
+                           "- neyi içe aktaracağımı nasıl seçerim?")
             return
 
         # Select data source
@@ -47,20 +46,20 @@ def import_data(bot: Bot, update):
             for mod in DATA_IMPORT:
                 mod.__import_data__(str(chat.id), data)
         except Exception:
-            msg.reply_text("An exception occured while restoring your data. The process may not be complete. If "
-                           "you're having issues with this, message @MarieSupport with your backup file so the "
-                           "issue can be debugged. My owners would be happy to help, and every bug "
-                           "reported makes me better! Thanks! :)")
+            msg.reply_text("Verilerinizi geri yüklerken bir istisna oluştu. İşlem tamamlanamayabilir. Eğer "
+                           "Bununla ilgili sorun yaşıyorsanız, yedekleme dosyanızla @sairbey mesajını gönderin, böylece "
+                           "sorun giderilebilir. Sahibim yardımcı olmaktan mutluluk duyacaktır ve her hata "
+                           "bildirilen beni daha iyi yapar! Teşekkürler! :)")
             LOGGER.exception("Import for chatid %s with name %s failed.", str(chat.id), str(chat.title))
             return
 
         # TODO: some of that link logic
         # NOTE: consider default permissions stuff?
-        msg.reply_text("Backup fully imported. Welcome back! :D")
+        msg.reply_text("Yedekleme tamamen içe aktarıldı. Tekrar hoş geldiniz !: D")
 
 
-@run_async
-@user_admin
+@sairbey 
+
 def export_data(bot: Bot, update: Update):
     msg = update.effective_message  # type: Optional[Message]
     msg.reply_text("")
